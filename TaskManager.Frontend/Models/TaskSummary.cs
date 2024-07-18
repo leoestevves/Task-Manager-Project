4a9a0d3462a3290c5178fadf46f8ce7c769fd4bf
leoestevves <leogoncalves60@gmail.com>
2024-07-18 14:17:31 -0300

    Using Razor syntax

---
+++ TaskManager.Frontend/Models/TaskSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Frontend.Models;
+
+public class TaskSummary
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+}
