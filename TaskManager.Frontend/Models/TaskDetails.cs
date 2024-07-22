8dc5bd8b3874d5b3466e68d11d67f84838a678e8
leoestevves <leogoncalves60@gmail.com>
2024-07-22 14:50:29 -0300

    Use Forms and bindings (1)

---
+++ TaskManager.Frontend/Models/TaskDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Frontend.Models;
+
+public class TaskDetails
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+}
