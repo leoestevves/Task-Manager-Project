08588597314842e517ed7b6f62bec10bb2ba005a
leoestevves <leogoncalves60@gmail.com>
2024-07-22 13:39:39 -0300

    Create and reuse components

---
+++ TaskManager.Frontend/Clients/TasksClient.cs
@@ -0,0 +1,30 @@
+﻿using TaskManager.Frontend.Models;
+
+namespace TaskManager.Frontend.Clients;
+
+public class TasksClient
+{
+    private readonly List<TaskSummary> tasks = 
+    [
+        new ()
+        {
+            Id = 1,
+            Title = "Limpar a casa",
+            Description = "Aproveitar o final de semana para limpar a casa inteira."
+        },
+        new ()
+        {
+            Id = 2,
+            Title = "Ir ao mercado",
+            Description = "Fazer compras pela manhã no mercado mais próximo."
+        },
+        new ()
+        {
+            Id = 3,
+            Title = "Finalizar projeto pessoal.",
+            Description = "Terminar o projeto de games nesse final de semana."
+        }
+    ];
+
+    public TaskSummary[] GetTasks() => tasks.ToArray();
+}
