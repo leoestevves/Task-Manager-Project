e129dbb2fcabf6036eb0616eea85d23df491f847
leoestevves <leogoncalves60@gmail.com>
2024-07-24 13:38:06 -0300

    Client-side validation

--- TaskManager.Frontend/Models/TaskDetails.cs
+++ TaskManager.Frontend/Models/TaskDetails.cs
@@ -1,8 +1,16 @@
-﻿namespace TaskManager.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Frontend.Models;
 
 public class TaskDetails
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Title { get; set; }
+
+    [Required]
+    [StringLength(150)]
     public required string Description { get; set; }
 }
